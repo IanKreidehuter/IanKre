13f984f22144eb0e7f5b716d368d292279c5c3e8
IanKreidehuter <froylanfahlanaditya@gmail.com>
2026-05-04 18:15:33 +0700

    Create updates.js

---
+++ data/updates.js
@@ -0,0 +1,17 @@
+const UPDATES = [
+  {
+    type: "image",
+    src: "assets/images/post1.jpg",
+    caption: "Just existing."
+  },
+  {
+    type: "video",
+    src: "assets/video/rev1.mp4",
+    caption: "Revelation"
+  },
+  {
+    type: "audio",
+    src: "assets/audio/song1.mp3",
+    caption: "Memory"
+  }
+];
