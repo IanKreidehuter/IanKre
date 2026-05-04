2273d1bd4be397fc9a7b16ef4d4ccb015130abd1
IanKreidehuter <froylanfahlanaditya@gmail.com>
2026-05-04 21:31:56 +0700

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -1,16 +1,27 @@
-const PROFILE_DATA = {
+const PROFILE = {
   name: "Andrian Kreidehüter",
+  alias: "Froylan",
+  age: "19",
+  origin: "German",
+
+  description: [
+    "Ian Kreideprinz is a 19-year-old from Germany.",
+    "He has a calm but introspective presence.",
+    "",
+    "A quiet soul carrying many storms inside.",
+    "empathetic, protective. Human."
+  ],
 
   psychological: [
     "Social Anxiety",
     "Depression",
     "C-PTSD",
-    "ASD",
-    "Anxious Attachment"
+    "ASD"
   ],
 
   music: [
     "Lost Boy — Ruth B.",
-    "Iris — Goo Goo Dolls"
+    "Iris — Goo Goo Dolls",
+    "Somewhere I Belong — Linkin Park"
   ]
 };
