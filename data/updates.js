8619b8ccc12f8b95ce50eab77cd6140485b47b7d
IanKreidehuter <froylanfahlanaditya@gmail.com>
2026-05-04 18:20:31 +0200

    Update updates.js

--- data/updates.js
+++ data/updates.js
@@ -2,13 +2,13 @@ const UPDATES = [
 
   {
     type: "image",
-    src: "assets/media/test1.jpg",
+    src: "assets/images/test1.jpg",
     caption: "SIGNAL LOG 01 — VISUAL ENTRY"
   },
 
   {
     type: "image",
-    src: "assets/media/test2.jpg",
+    src: "assets/images/test2.jpg",
     caption: "SIGNAL LOG 02 — MEMORY TRACE"
   }
 
