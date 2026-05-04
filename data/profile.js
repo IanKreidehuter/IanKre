22634b7524bcfeb9fc95327097b5286606f88d7c
IanKreidehuter <froylanfahlanaditya@gmail.com>
2026-05-04 18:14:40 +0700

    Create profile.js

---
+++ data/profile.js
@@ -0,0 +1,16 @@
+const PROFILE_DATA = {
+  name: "Andrian Kreidehüter",
+
+  psychological: [
+    "Social Anxiety",
+    "Depression",
+    "C-PTSD",
+    "ASD",
+    "Anxious Attachment"
+  ],
+
+  music: [
+    "Lost Boy — Ruth B.",
+    "Iris — Goo Goo Dolls"
+  ]
+};
