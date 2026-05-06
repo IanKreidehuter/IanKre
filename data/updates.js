c79fd0787646de61a1ba1089cb4747950d5f1d2d
IanKreidehuter <froylanfahlanaditya@gmail.com>
2026-05-06 19:56:57 +0700

    Update updates.js

--- data/updates.js
+++ data/updates.js
@@ -1,6 +1,12 @@
 const UPDATES = [
 
   {
+    type: "video",
+    src: "assets/video/daddyissues.mp4",
+    caption: "DON'T CARE HOW LONG IT TAKES. IF I WANT IT I'M GONNA GET IT."
+  },
+  
+  {
     type: "image",
     src: "assets/images/fallinloveagain.jpg",
     caption: "I didn’t expect to like you. At the beginning, I didn’t know you would come to mean so much to me—that I would give half of my life to this person and trust them to treat it like their own, trust them to take care of it better than even I could. I didn’t know that every goodbye would break my heart, shatter it into a million pieces and take it away—but that every hello would revive it, gluing it back together piece by piece, checking carefully that nothing is damaged. I didn’t plan on you meaning so much, but I guess the most beautiful things do come naturally, because you can’t plan for something like this. You can’t plan for someone to show up and make every moment brighter, make your heart feel that much lighter without even trying. You can’t plan for someone to come along and offer to piece you back together, to heal you without even asking for permission, for no reason other than because they care. Maybe that’s why this works—why it makes sense that you mean so much—because it doesn’t take any effort at all. I didn’t have a single thought in my head when you came along at the perfect time and completely took me off guard. No, I didn’t expect to like you the way I do, but that’s what makes this love feel so true."
